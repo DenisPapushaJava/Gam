ec846811edee18d4ccf6642de77964481dcf07f1
Денис <denis-papusha@rambler.ru>
2021-05-17 07:39:47 +0300

    GitHub

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Game");
         JFrame window = new JFrame("Крестики-Нолики");
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setSize(400, 400);
@@ -14,7 +13,5 @@ public class Main {
         window.setVisible(true);
         TicTacToe game = new TicTacToe();
         window.add(game);
-        System.out.println("End");
-
     }
 }
